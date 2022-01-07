fa0bc3e8a15cc77b2026af736541cdc8f91fa273
Juliana Valle <mfrasao@hotmail.com>
2022-01-07 13:46:12 -0300

    finalização do primério módulo do ignite nodejs

--- src/index.js
+++ src/index.js
@@ -2,15 +2,23 @@ const express = require('express');
 
 const app = express();
 
+app.use(express.json());
+
 app.get("/coursers",(request, response) => {
+    const query = request.query;
+    console.log(query);
     return response.json(["curso 1","curso 2", "curso 3"]);
 });
 
 app.post("/coursers",(request, response) => {
+    const body = request.body;
+    console.log(body);
     return response.json(["curso 1","curso 2", "curso 3", "curso 4"])
 });
 
 app.put("/coursers/:id",(request,response) => {
+    const { id } = request.params;
+    console.log(id);    
     return response.json(["curso 6","curso 2", "curso 3", "curso 4"])
 });
 
